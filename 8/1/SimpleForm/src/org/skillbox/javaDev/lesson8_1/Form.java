134a8327b50eb8d015966a9a3dd970f062bf8fb6
Light-Bearing <hilo@list.ru>
2020-03-30 18:15:27 +0300

    commit SimpleForm 8.1

---
+++ 8/1/SimpleForm/src/org/skillbox/javaDev/lesson8_1/Form.java
@@ -0,0 +1,14 @@
+package org.skillbox.javaDev.lesson8_1;
+
+import javax.swing.*;
+
+public class Form {
+    private JPanel rootPanel;
+    private JTextField textField1;
+    private JButton button1;
+    private JLabel surname;
+
+    public JPanel getRootPanel() {
+        return rootPanel;
+    }
+}
