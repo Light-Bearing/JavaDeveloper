91cc301d02cb405f701f6600270027f857dff868
LightBearing <hilo@list.ru>
2020-03-30 13:23:39 +0300

    again commit

---
+++ 6/6.2/inheritance/src/com/skillbox/inheritance/Main.java
@@ -0,0 +1,12 @@
+package com.skillbox.inheritance;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Furniture table = new Table();
+        Furniture chair = new Chair();
+
+
+    }
+}
