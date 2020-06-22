9ec5b800e57286c29293a37ccbb8fc3a0394737f
LightBearing <hilo@list.ru>
2020-06-22 20:05:47 +0300

    8.4 завершено

--- 8/4/FIOSelector/src/org/skillbox/javaDev/lesson8_4/ShortForm.java
+++ 8/4/FIOSelector/src/org/skillbox/javaDev/lesson8_4/ShortForm.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import static org.skillbox.javaDev.lesson8_4.TextControl.*;
 
 public class ShortForm {
     private JPanel rootPanel;
@@ -24,6 +25,15 @@ public class ShortForm {
             public void keyReleased(KeyEvent e) {
                 super.keyReleased(e);
                 if (e.getKeyCode() == KeyEvent.VK_ENTER && e.isControlDown()) onClickButton();
+            }
+        });
+
+        textFIO.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                if (!valideInput(PATTERN_FIO, textFIO.getText() + e.getKeyChar()))
+                    e.consume();
             }
         });
     }
---
+++ 8/4/FIOSelector/src/org/skillbox/javaDev/lesson8_4/TextControl.java
@@ -0,0 +1,14 @@
+package org.skillbox.javaDev.lesson8_4;
+
+import java.util.regex.Pattern;
+
+public class TextControl {
+    public static final String PATTERN_SURNAME = "[А-ЯЁ&&[^ЪЬЫ]]{1}[а-яё]*-?[а-яё]*";
+    public static final String PATTERN_NAME = "[А-ЯЁ&&[^ЪЬЫ]]{1}[а-яё]*";
+    public static final String PATTERN_PATRONYMIC = "[А-ЯЁ&&[^ЪЬЫ]]{1}[а-яё]*";
+    public static final String PATTERN_FIO = "[А-ЯЁ&&[^ЪЬЫ]]{1}[а-яё]*-?[а-яё]*(\\s++[А-ЯЁ&&[^ЪЬЫ]]{0,1}[а-яё]*){0,2}";
+
+    public static boolean valideInput(String pattern, String input) {
+        return Pattern.compile(pattern).matcher(input).matches();
+    }
+}
