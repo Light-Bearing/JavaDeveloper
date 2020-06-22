ad6f66bcedc82b9e51e5fd2067c2f9b97ce9b4a9
LightBearing <hilo@list.ru>
2020-06-22 20:11:29 +0300

    8.5 адаптирован

--- 8/5/SimpleForm2/src/Form.java
+++ 8/5/SimpleForm2/src/Form.java
@@ -1,21 +1,55 @@
 import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Form {
     private JPanel mainPanel;
     private JPanel labelPanel;
     private JPanel editPanel;
 
+    private JTextField JTSurname = new JTextField();
+    private JTextField JTName = new JTextField();
+    private JTextField JTPatronymic = new JTextField();
+
     {
-        mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.LINE_AXIS));
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.LINE_AXIS));
         labelPanel.add(new JLabel("Фамилия:")).setPreferredSize(labelPanel.getPreferredSize());
         labelPanel.add(new JLabel("Имя:")).setPreferredSize(labelPanel.getPreferredSize());
         labelPanel.add(new JLabel("Отчество:")).setPreferredSize(labelPanel.getPreferredSize());
-        editPanel.add(new JTextField()).setPreferredSize(editPanel.getPreferredSize());
-        editPanel.add(new JTextField()).setPreferredSize(editPanel.getPreferredSize());
-        editPanel.add(new JTextField()).setPreferredSize(editPanel.getPreferredSize());
+
+        editPanel.add(JTSurname).setPreferredSize(editPanel.getPreferredSize());
+        editPanel.add(JTName).setPreferredSize(editPanel.getPreferredSize());
+        editPanel.add(JTPatronymic).setPreferredSize(editPanel.getPreferredSize());
     }
 
-    public JPanel getRootPanel(){
+    public Form() {
+        JTSurname.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                if (!TextControl.valideInput(TextControl.PATTERN_SURNAME, JTSurname.getText() + e.getKeyChar()))
+                    e.consume();
+            }
+        });
+        JTName.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                if (!TextControl.valideInput(TextControl.PATTERN_NAME, JTName.getText() + e.getKeyChar()))
+                    e.consume();
+            }
+        });
+        JTPatronymic.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                if (!TextControl.valideInput(TextControl.PATTERN_PATRONYMIC, JTPatronymic.getText() + e.getKeyChar()))
+                    e.consume();
+            }
+        });
+    }
+
+    public JPanel getRootPanel() {
         return mainPanel;
     }
 }
